e474b561e241c64324a975525a2fc7b695d81156
Derek Kaye <github@dezzanet.co.uk>
2020-03-18 09:21:09 +0000

    Get url parameter

--- fireglass-fix.user.js
+++ fireglass-fix.user.js
@@ -6,11 +6,17 @@
 // @run-at       document-start
 // @updateURL    https://github.com/dezzak/fireglass-fix/raw/master/fireglass-fix.user.js
 // @downloadURL  https://github.com/dezzak/fireglass-fix/raw/master/fireglass-fix.user.js
-// @version      0.1
+// @version      0.2
 // ==/UserScript==
 
 (function() {
   'use strict';
-  const fireglassUrl = window.location;
-  console.debug("Got URL", fireglassUrl);
+  const searchParamsRaw = window.location.search;
+  if (searchParamsRaw) {
+    const searchParams = new URLSearchParams(searchParamsRaw.substr(1));
+    const targetUrl = searchParams.get('url');
+    if (targetUrl) {
+      console.debug(targetUrl);
+    }
+  }
 })();
