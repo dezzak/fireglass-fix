4e2cba63efa35840eea44392a13d9488896751ea
Derek Kaye <github@dezzanet.co.uk>
2020-03-18 10:29:08 +0000

    Make redirect work for whitelist

--- fireglass-fix.user.js
+++ fireglass-fix.user.js
@@ -6,17 +6,28 @@
 // @run-at       document-start
 // @updateURL    https://github.com/dezzak/fireglass-fix/raw/master/fireglass-fix.user.js
 // @downloadURL  https://github.com/dezzak/fireglass-fix/raw/master/fireglass-fix.user.js
-// @version      0.2
+// @version      1.0
 // ==/UserScript==
 
 (function() {
   'use strict';
+
+  const whitelist = [
+    'https://app.teammood.com/',
+    'https://sainsburys-confluence.valiantys.net/',
+    'https://sainsburys-jira.valiantys.net/',
+  ];
+
   const searchParamsRaw = window.location.search;
   if (searchParamsRaw) {
     const searchParams = new URLSearchParams(searchParamsRaw.substr(1));
     const targetUrl = searchParams.get('url');
     if (targetUrl) {
-      console.debug(targetUrl);
+      whitelist.forEach(permitted => {
+        if (targetUrl.startsWith(permitted)) {
+          window.location = targetUrl;
+        }
+      })
     }
   }
 })();
